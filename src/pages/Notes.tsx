7d8cdc6d58f9d1ba648bd1d8028bb4d3eebcae43
Ana Luiza Rodrigues Chiamenti <analuizarodrigueschiamenti@gmail.com>
2023-05-05 00:02:38 -0300

    cores

--- src/pages/Notes.tsx
+++ src/pages/Notes.tsx
@@ -53,64 +53,76 @@ const Notes: React.FC = () => {
     return (
         <Grid container sx={{ width: '100%', height: '100vh' }}>
             <ResponsiveAppBar />
-            <Box width="100%" height="100%" sx={{ display: 'flex', justifyContent: 'center' }} paddingTop="4.5rem">
-                <Grid container width="100%" justifyContent="center" marginTop="15px">
-                    <Grid container width="100%">
-                        {listTaks.map(note => (
-                            <Grid item display="flex" flexDirection="row" key={note?.id}>
-                                <Card
-                                    sx={{
-                                        width: '300px',
-                                        height: '200px',
-                                        margin: '20px',
-                                        marginBottom: '0px !important'
-                                    }}
-                                >
-                                    <CardContent>
-                                        <Typography gutterBottom variant="h5" component="div">
-                                            {note.title}
-                                        </Typography>
+            <Box width="100%" bgcolor="#EFF7CF" paddingTop="4.5rem">
+                <Grid container width="100%">
+                    {listTaks.map(note => (
+                        <Grid
+                            item
+                            xs={12}
+                            sm={6}
+                            md={3}
+                            key={note?.id}
+                            display="flex"
+                            justifyContent="center"
+                            flexDirection="row"
+                        >
+                            <Card
+                                sx={{
+                                    width: '300px',
+                                    height: '150px',
+                                    marginY: '25px',
+                                    marginX: '15px'
+                                }}
+                            >
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="div">
+                                        {note.title}
+                                    </Typography>
 
-                                        <Typography variant="body2" color="text.secondary">
-                                            {note.description}
-                                        </Typography>
-                                    </CardContent>
-                                    <CardActions sx={{ display: 'flex', height: '150px' }}>
-                                        <IconButton onClick={() => handleEdit(note)}>
-                                            <EditIcon />
-                                        </IconButton>
-                                        <IconButton onClick={() => handleDelete(note)}>
-                                            <DeleteIcon />
-                                        </IconButton>
-                                    </CardActions>
-                                </Card>
-                            </Grid>
-                        ))}
-                    </Grid>
-                    {openModalEdit && (
-                        <ModalInputsEdit
-                            openModal={openModalEdit}
-                            actionConfirm={addNotesEdit}
-                            actionCancel={handleCloseEdit}
-                            task={editedTaks}
-                        />
-                    )}
+                                    <Typography variant="body2" color="text.secondary" noWrap>
+                                        {note.description}
+                                    </Typography>
+                                </CardContent>
+                                <CardActions sx={{ display: 'flex', height: '40%' }}>
+                                    <IconButton aria-label="edit" onClick={() => handleEdit(note)}>
+                                        <EditIcon />
+                                    </IconButton>
+                                    <IconButton aria-label="delete" onClick={() => handleDelete(note)}>
+                                        <DeleteIcon />
+                                    </IconButton>
+                                </CardActions>
+                            </Card>
+                        </Grid>
+                    ))}
                 </Grid>
-                <Fab
-                    onClick={openModalImput}
-                    color="info"
-                    aria-label="add"
-                    sx={{
-                        position: 'fixed',
-                        right: '20px',
-                        bottom: '20px',
-                        bgcolor: '#222'
-                    }}
-                >
-                    <AddIcon />
-                </Fab>
-                <ModalInputs openModal={openAdd} actionConfirm={addNotes} actionCancel={handleClose} />
             </Box>
+
+            <Fab
+                onClick={openModalImput}
+                color="info"
+                aria-label="add"
+                sx={{
+                    position: 'fixed',
+                    right: '20px',
+                    bottom: '20px',
+                    bgcolor: '#222',
+                    width: '80px',
+                    height: '80px',
+                    boxShadow: '5px 10px 20px rgba(0, 0, 0, 0.301), 5px 10px 20px rgba(0, 0, 0, 0.301);'
+                }}
+            >
+                {<AddIcon fontSize="large" />}
+            </Fab>
+            {openModalEdit && (
+                <ModalInputsEdit
+                    openModal={openModalEdit}
+                    actionConfirm={addNotesEdit}
+                    actionCancel={handleCloseEdit}
+                    task={editedTaks}
+                />
+            )}
+
+            <ModalInputs openModal={openAdd} actionConfirm={addNotes} actionCancel={handleClose} />
         </Grid>
     );
 };
